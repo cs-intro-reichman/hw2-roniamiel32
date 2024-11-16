0cb0d00ef7c8a2d1097c214d74a95c13ac227e02
Roni <roni.amiel@post.runi.ac.il>
2024-11-17 00:18:14 +0200

    Completed the mission

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,22 @@
-// Prints a crowd cheering output.
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
-        }
-}
+                String Cheer = args[0];
+                int printCount = Integer.parseInt(args[1]);
+                String specialLetters = "AEFHILMNORSX";
+                for (int i = 0; i < Cheer.length(); i++) {
+                        char currentLetter = Cheer.charAt(i);
+                        String rule = specialLetters.indexOf(currentLetter) >= 0 ? "an" : "a";
+                        System.out.println("Give me " + rule + " " + currentLetter + ": " + currentLetter + "! ");                       
+                }
+                System.out.println("What does that spell?");
+                for (int i = 0; i < printCount; i++) {
+                        System.out.println(Cheer + "!!!");
+                }
+               
+    }
+    }
+
+
+
+
