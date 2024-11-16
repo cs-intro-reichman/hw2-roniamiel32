77451dcf6d113712f138948a0d5b09512f947a5d
Roni <roni.amiel@post.runi.ac.il>
2024-11-17 00:57:52 +0200

    Completed the mission

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,7 @@ public class Cheers {
                 for (int i = 0; i < Cheer.length(); i++) {
                         char currentLetter = Cheer.charAt(i);
                         String rule = specialLetters.indexOf(currentLetter) >= 0 ? "an" : "a";
-                        System.out.print("Give me " + rule + " " + currentLetter + ": " + currentLetter + "! ");                       
+                        System.out.println("Give me " + rule + " " + currentLetter + ": " + currentLetter + "! ");                       
                 }
                 System.out.println("What does that spell?");
                 for (int i = 0; i < printCount; i++) {
