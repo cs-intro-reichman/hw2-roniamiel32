b56a1282f4e8d14f079d451c18a4128ec726276e
Roni <roni.amiel@post.runi.ac.il>
2024-11-16 16:06:49 +0200

    Completed the mission

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,24 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-    }
+        int time = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+       int totalMinutes = (time * 60) + minutes + minutesToAdd;
+       int totalHour = totalMinutes / 60;
+       int newHour = totalHour % 24;
+       int newMinutes = totalMinutes - (totalHour * 60);
+    //   System.out.println(totalMinutes);
+       // System.out.println(totalHour);
+     //   System.out.println(newHour);
+      //  System.out.println(newMinutes);
+      if (newMinutes < 10) {
+        System.out.println(newHour + ":0" + newMinutes);
+    } else {
+         if (newHour < 10) {
+        System.out.println("0" + newHour + ":" + newMinutes);
+    } else {
+       System.out.println(newHour + ":" + newMinutes);
+       }
+    } 
+}
 }
