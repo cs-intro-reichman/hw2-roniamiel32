94d11b7fbc5593c06a559cc129c1391a30e456e5
Roni <roni.amiel@post.runi.ac.il>
2024-11-21 21:44:41 +0200

    Completed the mission

--- Cheers.java
+++ Cheers.java
@@ -1,18 +1,18 @@
 
 public class Cheers {
         public static void main(String[] args) {
-          String Cheer = args[0]  ;
+          String cheer = args[0];
           int printCount = Integer.parseInt(args[1]);
           String specialLetters = "AaEeFfHhIiLlMmNnOoRrSsXx";
-                for (int i = 0; i < Cheer.length(); i++) {
-                 char currentLetter = Cheer.charAt(i);
-                 currentLetter = Character.toUpperCase(currentLetter);
-                 String rule = (specialLetters.indexOf(Character.toUpperCase(currentLetter)) >= 0) ? "an" : "a";
-                System.out.println("Give me " + rule + " " + currentLetter + ": " + currentLetter + "!");
+          for (int i = 0; i < cheer.length(); i++) {
+         char currentLetter = cheer.charAt(i);
+         currentLetter = Character.toUpperCase(currentLetter);
+         String rule = (specialLetters.indexOf(Character.toUpperCase(currentLetter)) >= 0) ? "an " : "a  ";
+        System.out.println("Give me " + rule + currentLetter + ": " + currentLetter + "!");
         }
-                 System.out.println("What does that spell?");
-                 for (int i = 0; i < printCount; i++) {
-                System.out.println(Cheer.toUpperCase()+ "!!!");
+         System.out.println("What does that spell?");
+         for (int i = 0; i < printCount; i++) {
+        System.out.println(cheer.toUpperCase()+ "!!!");
                 }
         }   
     }
