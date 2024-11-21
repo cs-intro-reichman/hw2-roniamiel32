c5888adfcc8a6dc29ca66cf45806513ad93eb394
Roni <roni.amiel@post.runi.ac.il>
2024-11-21 16:30:19 +0200

    Completed the mission

--- Cheers.java
+++ Cheers.java
@@ -1,21 +1,24 @@
 
 public class Cheers {
         public static void main(String[] args) {
-                String Cheer = args[0].toUpperCase() ;
+                String Cheer = args[0]  ;
                 int printCount = Integer.parseInt(args[1]);
-                String specialLetters = "AaEeFfHhIiLlMmNnOoRrSsXx";
+                String specialLetters = "AEFHILMNORSX";
                 for (int i = 0; i < Cheer.length(); i++) {
-                        char currentLetter = Cheer.charAt(i);
-                        String rule = specialLetters.indexOf(currentLetter) >= 0 ? "an" : "a";
-                        System.out.println("Give me " + rule + " " + currentLetter + ": " + currentLetter + "! ");                       
+                 char currentLetter = Cheer.charAt(i);
+                 if (currentLetter >= 'a' && currentLetter <= 'z') {
+                        currentLetter = (char)(currentLetter -32);
                 }
-                System.out.println("What does that spell?");
-                for (int i = 0; i < printCount; i++) {
-                        System.out.println(Cheer + "!!!");
+                 String rule = specialLetters.indexOf(currentLetter) >= 0 ? "an" : "a";
+                 System.out.println("Give me " + rule + " " + currentLetter + ": " + currentLetter + "! ");                       
                 }
-               
+                 System.out.println("What does that spell?");
+                 for (int i = 0; i < printCount; i++) {
+                System.out.println(Cheer + "!!!");
+                }
+        }   
     }
-    }
+    
 
 
 
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,33 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		int j = 0;
+		int sum = 2;
+		int count = 1;
+		for (int i = 0; i < seed; i++){
+			j++;
+			System.out.print(j + " "); 
+			while (sum > 1) { 
+				if (j % 2 == 0 ) {
+				j = j / 2;
+		 	    System.out.print(j + " "); 
+				sum = j;	
+				count ++;
+		 }else if (seed % 2 != 0){
+			j = (j * 3) + 1;
+			System.out.print(j + " ");
+			sum = j;	
+				count ++;
+		 }
+			}
+			System.out.print("count " + count); 
+			System.out.println(" ");
+			
+			//break;
+		 }
+		}
 	}
-}
+
+
+
